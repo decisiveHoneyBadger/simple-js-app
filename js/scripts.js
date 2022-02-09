df49583ee4ae20897f1e213bb8fb3faea0db41c3
decisiveHoneyBadger <k.guevenc@web.de>
2022-02-10 00:02:18 +0100

    clean up script and alert box
    If applied, this commit will have cleaned the unnecessary code above, added an alert pop-up and displays my favorite food

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+alert('Hello World');
 
-let simpleAddition = 2 + 2:
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+let favoriteFood = 'Burger';
+document.write(favoriteFood);
