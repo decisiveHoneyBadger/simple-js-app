a5f89ec8644baff98454d344fb54b6f826c27463
decisiveHoneyBadger <k.guevenc@web.de>
2022-03-10 16:19:26 +0100

    created a DOM interaction
    If applied, this commit will have created a DOM interaction

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,6 @@ The loop is set until reaching the same length as the array. Because the item "E
 after it.
 */
 
-
-
 function divide(dividend, divisor){
   if(divisor === 0){
     return "You’re trying to divide by zero."
@@ -22,6 +20,7 @@ function divide(dividend, divisor){
   }
 }
 
+// this is the beginning of the IIFE
 let pokemonRepository = (function () {
   let pokemonList = [
     {
@@ -40,26 +39,110 @@ let pokemonRepository = (function () {
       name: "Jynx",
       height: 1.4,
       weight: 40.6,
-      type: ["psychic", "ice"],
+      types: ["psychic", "ice"],
     },
+    {
+      name: "Bulbasar",
+      height: 0.7,
+      weight: 6.9,
+      types: ["grass", "poison"],
+    }
   ];
 
-  return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
-      return pokemonList;
+  function getAll() {
+    return pokemonList;
+  }
+ 
+  /**
+   * adds only pokemons provided they are valid objects
+   * and validates all parameters necessary
+   * 
+   * @param {*} pokemon this is the pokemon object
+   */
+  function addValidate(pokemon) {
+    if (typeof pokemon === 'object' && !Array.isArray(pokemon)) {
+      if ((Object.keys(pokemon)[0] === 'name' && typeof Object.values(pokemon)[0] === 'string') &&
+          (Object.keys(pokemon)[1] === 'height' && typeof Object.values(pokemon)[1] === 'number') &&
+          (Object.keys(pokemon)[2] === 'weight' && typeof Object.values(pokemon)[2] === 'number') &&
+          (Object.keys(pokemon)[3] === 'types' && Array.isArray(Object.values(pokemon)[3]))) {
+            pokemonList.push(pokemon);
+      } else {
+        window.alert('this is not a valid pokemon object');
+      }
     }
+  }
+
+/**
+ * finds the pokemon object based on the given search term 
+ * 
+ * @param {*} searchTerm to look for the key name and its value 
+ * @returns the searched pokemon object
+ */
+function findPokemon(searchTerm) {
+  return pokemonList.filter(o => 
+    o['name'].toLowerCase().includes(searchTerm.toLowerCase()));
+}
+/**
+ * adds a pokemon list item
+ * @param {*} pokemon to add to the list
+ */
+function addListItem(pokemon) {
+  let pokemonList = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add('button');
+  pokemonList.appendChild(listItem);
+  pokemonList.appendChild(button);
+
+ // adding an event listener for button (variable)
+  button.addEventListener("click", showDetails);
+/**
+ * adds the event listener to the newly created button
+ * @param {*} button refers to the declared button above
+ * @param {*} pokemon refers to the pokemon object
+ */
+function showDetails(click, button, pokemon) {
+  alert(`Hey, it's me`);
+}
+}
+
+// adds a function
+function showDetails(pokemon) {
+  console.log(pokemon);
+}
+// makes the functions public (usable) and accessable
+  return {
+    addValidate: addValidate,
+    getAll: getAll,
+    findPokemon: findPokemon,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-// I replaced the foor loop with this forEach loop
+// adding a valid pokemon to pokemonList in pokemonRepository (IIFE)
+pokemonRepository.addValidate({ name:'pokemon', height: 1.2, weight: 20.6, types: ['fire']});
+console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.findPokemon('Bul'));
+pokemonRepository.addValidate({ name: 1, height: 1.2, weight: 20.6, types: ['fire']});
+
+// DOM manipulation
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + ' is ' + pokemon.height + ' tall ' + ( '<br>'));
+  pokemonRepository.addListItem(pokemon);
 });
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Bulbasaur' });
-console.log(pokemonRepository.getAll());
+
+
+
+
+/*
+
+*/
+
+
+
+
+
+
 
